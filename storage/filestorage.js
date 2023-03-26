c05fe5a1bc45beb69ac22a48cbb7c773519031bd
Diabene <addodiabene69@gmail.com>
2023-03-26 23:16:05 +0000

    storage

---
+++ storage/filestorage.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const args = process.argv.splice(2);
+const command = args.shift();
+const taskdescription = args.join(' ');
+const file = path.join(process.cwd(), './tasks');
+
+function listFile(file) {console.log('all files ', file)};
+function addFile(file, description) {console.log(`"${description}" added to files into directory ${file}`)};
+
+switch(command) {
+    case 'list':
+        listFile(file);
+        break;
+    case 'add':
+        addFile(file, taskdescription);
+        break;
+    default:
+        console.log('Usage: ' + process.argv[0] + ' list|add [taskdescription]');
+}
